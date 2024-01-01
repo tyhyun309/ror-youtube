32e421ffad79d59e5afa12056e379229bc32b03a
Yin Lee <leesrrh@gmail.com>
2024-01-01 22:00:50 +0900

    image & video

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :desc)
+      params.require(:post).permit(:title, :desc, :image, :video)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  has_one_attached :image
+  has_one_attached :video
 end
